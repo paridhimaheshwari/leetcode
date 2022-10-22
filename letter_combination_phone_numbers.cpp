ea8190ec3c72f04752473b264d4fa1108e66edf3
Paridhi Maheshwari <maheshwariparidhi16@gmail.com>
2022-10-22 14:50:24 +0530

    tring out

--- letter_combination_phone_numbers.cpp
+++ letter_combination_phone_numbers.cpp
@@ -10,21 +10,24 @@ public:
     vector<string> letterCombinations(string digits) {
         if(digits.length() > 4){
             cout <<  "greater than 4. " <<endl;
-            return;
+            return vector<string> () ;
         }
         for(int i=0 ; i<digits.length(); i++){
             if(digits[i] > '9' || digits[i] < '2'){
-                return;
+                return vector<string> ();
             }
         }
 
         for(int j = 0; j<digits.length(); j++){
-            digit[i];
+            cout << digits[j];
         }
 
         //creating a map of digits to character
         
         //
+
+
+        return vector<string> ();
     }
 
     
@@ -39,6 +42,7 @@ int main(){
     Solution s1;
     cout << "Enter digits: " <<endl;
     string digits;
+    cin >> digits;
     vector<string> v1;
     v1 = s1.letterCombinations(digits);
     print(v1);
