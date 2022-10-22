ceadd64178c4eeb16e1f107e5244738835db13ab
Paridhi Maheshwari <maheshwariparidhi16@gmail.com>
2022-10-22 17:18:24 +0530

    solved using recursion

--- letter_combination_phone_numbers.cpp
+++ letter_combination_phone_numbers.cpp
@@ -2,42 +2,63 @@
 #include<vector>
 #include<string>
 
+
 using namespace std;
 
 
 class Solution {
-public:
-    vector<string> letterCombinations(string digits) {
-        if(digits.length() > 4){
-            cout <<  "greater than 4. " <<endl;
-            return vector<string> () ;
-        }
-        for(int i=0 ; i<digits.length(); i++){
-            if(digits[i] > '9' || digits[i] < '2'){
-                return vector<string> ();
-            }
-        }
 
-        for(int j = 0; j<digits.length(); j++){
-            cout << digits[j];
-        }
-
-        //creating a map of digits to character
+    private: 
         
-        //
-
-
-        return vector<string> ();
-    }
-
-    
+    public:
+        vector<string> letterCombinations(string digits) {
+            int x =digits.length() ;
+            if(x > 4){
+                cout <<  "greater than 4. " <<endl;
+                return vector<string> () ;
+            }
+            for(int i=0 ; i<x; i++){
+                if(digits[i] > '9' || digits[i] < '2'){
+                    return vector<string> ();
+                }
+            }
+            
+            const string map[] = {"0", "1", "abc", "def", "ghi" , "jkl" , 
+            "mno", "pqrs", "tuv", "wxyz"};
+            vector<string> arr1;
+            char first=digits[0];
+            string curr_charmap = map[first - '0'];
+            vector<string> ret_arr;
+            if(x > 1){
+                arr1 = letterCombinations(digits.substr(1,x-1));
+                for(int i = 0; i<curr_charmap.length(); i++){
+                    for(int j = 0; j<arr1.size(); j++){
+                        string new_one = arr1.at(j) + curr_charmap[i];
+                        ret_arr.push_back(new_one);
+                    }
+                }
+                return ret_arr;
+            }else{ //base case
+                cout << "I am in base case: " << digits << endl;
+                for(int i = 0; i < curr_charmap.length();i++) {
+                    cout << "current char: " << curr_charmap[i] <<endl;
+                    string new_one ="";
+                    new_one += curr_charmap[i];
+                    cout << "String: " << new_one << endl;
+                    ret_arr.push_back(new_one);
+                }
+                return ret_arr;
+            }
+            return vector<string> ();
+        }    
 };
 
-void print(vector<string> v){
-
-
+void print(vector<string> arr) {
+    for(string s:arr) {
+        cout << s << ", ";
+    }
+    cout << endl;
 }
-
 int main(){
     Solution s1;
     cout << "Enter digits: " <<endl;
